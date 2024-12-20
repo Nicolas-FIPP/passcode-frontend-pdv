8989d0b1879f1b0120cadeb63c61e10964bf2aa2
victorhugoRUB <victor.santos@rubcube.com>
2024-12-19 22:26:06 -0300

    feat: colors

---
+++ src/app/themes/colors.tsx
@@ -0,0 +1,6 @@
+export class PDVColors {
+    static readonly mainBlue = '#135884';
+    static readonly white = '#ffffff';
+    static readonly lightGray = '#E8E8E8';
+    static readonly black = '#000000';
+}
