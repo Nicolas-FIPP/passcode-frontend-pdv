525f9cffc90134ea4c83f1383188471bd2fac5df
victorhugoRUB <victor.santos@rubcube.com>
2024-12-19 22:26:22 -0300

    feat: colors

--- src/app/themes/icons.tsx
+++ src/app/themes/icons.tsx
@@ -1,3 +1,7 @@
+import ProfileIcon from '../assets/icons/profile.svg';
+import TitlePdvIcon from '../assets/icons/title_pdv.svg';
+
 export class PDVIcons {
-    static readonly profileIcon: string = '../assets/icons/profile.svg';
+    static readonly ProfileIcon = ProfileIcon;
+    static readonly TitlePdvIcon = TitlePdvIcon;
 }
