4af6cd250c7b17e32f3758e2159166d3566d75b1
victorhugoRUB <victor.santos@rubcube.com>
2024-12-19 22:26:53 -0300

    feat: shopping screen

--- src/app/pages/home/home-style.tsx
+++ src/app/pages/home/home-style.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import styled from "styled-components";
+import { PDVIcons } from "@/app/themes/icons";
+import { PDVColors } from "@/app/themes/colors";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  background-color: aliceblue;
   height: 100vh;
 `;
 
@@ -15,39 +16,137 @@ export const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 100vh;
-  padding: 0 2rem;
-  background-color: #333;
+  flex-direction: column;
+  height: 100%;
+  padding: 1.5rem 1rem;
+  background-color: ${PDVColors.mainBlue};
   color: white;
 `;
 
 export const MainBlock = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
-  background-color: aquamarine;
-  padding: 2rem;
-  height: 100vh;
+  background-color: ${PDVColors.lightGray};
+  padding: 0 0 2rem 0;
+  height: 100%;
   width: 100%;
 `;
 
-export const InfoBlock = styled.div`
+export const InfoListBlock = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: start;
+  background-color: ${PDVColors.white};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  height: 100%;
+  width: 60%;
+`;
+
+export const InfoValueBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  background-color: ${PDVColors.lightGray};
+  padding: 1.5rem;
+  height: 100%;
+  width: 40%;
+`;
+
+export const DivIconImage = styled.div`
+  display: flex;
+  align-items: center;
   justify-content: center;
-  background-color: beige;
-  padding: 2rem;
-  height: 100vh;
+`;
+
+export const DivColumnWithSpace = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+export const DivRowWithSpace = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 1.5rem;
+`;
+
+export const ProfileIcon = styled(PDVIcons.ProfileIcon)`
+  width: 2.5rem;
+`;
+
+export const TitleImage = styled(PDVIcons.TitlePdvIcon)`
+  width: 4rem;  
+`
+
+export const ExitButton = styled.button`
+  background-color: ${PDVColors.white};
+  color: ${PDVColors.mainBlue};
+  padding: 1rem 1.5rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  font-size: 1rem;
+`;
+
+export const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  color: ${PDVColors.black};
+  font-size: large;
+
+  th {
+    background-color: ${PDVColors.lightGray};
+    padding: 1.5rem;
+  }
+
+  td {
+    padding: 1.5rem;
+  }
+
+  tr:nth-child(even) {
+    background-color: ${PDVColors.lightGray};
+  }
+
+  tr:nth-child(odd) {
+    background-color: ${PDVColors.white};
+  }
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
   width: 100%;
 `;
 
-// export const IconImage = styled.div`
-//   width: 100px;
-//   height: 100px;
-//   cursor: pointer;
-//   /* background-color: aliceblue; */
-//   background-image: url('../assets/images/profile.png');
-//   margin: 1rem;
-// `;
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+
+  label {
+    font-size: large;
+    color: ${PDVColors.black};
+  }
+
+  input {
+    height: 40px;
+    color: ${PDVColors.black};
+    padding: 1.5rem;
+    background-color: ${PDVColors.white};
+    border: transparent;
+    border-radius: 1.2rem;
+    font-size: large;
+  }
+  span {
+    font-size: 16px;
+    color: #333;
+  }
+`;
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,20 +1,76 @@
 import { PDVIcons } from "@/app/themes/icons";
-import { PDVImages } from "@/app/themes/images";
 import styles from "../../page.module.css";
 
-import { Container, InfoBlock, MainBlock, NavBar } from "./home-style";
+import { Container, DivColumnWithSpace, DivIconImage, DivRowWithSpace, ExitButton, FormContainer, InfoListBlock, InfoValueBlock, InputGroup, MainBlock, NavBar, ProfileIcon, StyledTable, TitleImage } from "./home-style";
 
 export default function Home() {
   return (
     <Container> 
       <NavBar>
+        <DivIconImage>
+          <TitleImage/>
+        </DivIconImage>
+        <DivColumnWithSpace>
+          <DivIconImage>
+            <ProfileIcon/>
+          </DivIconImage>
+          <ExitButton>
+            Sair
+          </ExitButton>
+        </DivColumnWithSpace>
       </NavBar>
       <MainBlock>
-
+        <InfoListBlock>
+          <StyledTable>
+            <thead>
+              <tr>
+                <th align="center">QTD</th>
+                <th align="left">Produto</th>
+                <th align="center">Vl. Unit</th>
+                <th align="left">Total Produto</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+                {Array.from({ length: 10 }).map((_, index) => (
+                <tr key={index}>
+                  <td align="center">1</td>
+                  <td align="left">CAMISINHA TAMANHO PP EREC...</td>
+                  <td align="center">1</td>
+                  <td align="left">R$ 10,00</td>
+                  <td align="center"></td>
+                </tr>
+                ))}
+            </tbody>
+          </StyledTable>
+        </InfoListBlock>
+        <InfoValueBlock>
+          <FormContainer>
+            <InputGroup>
+              <label>Ṕroduto</label>
+              <input type="text" placeholder="Código de barras ou nome do produto" />
+            </InputGroup>
+            <DivRowWithSpace>
+                <InputGroup>
+                <label>Quantidade</label>
+                <input type="number" placeholder="0" style={{ textAlign: 'center' }} />
+                </InputGroup>
+              <InputGroup>
+                <label>Valor Unitário</label>
+                <input type="number" placeholder="0,00" style={{ textAlign: 'center' }} />
+              </InputGroup>
+            </DivRowWithSpace>
+            <InputGroup>
+              <label>Desconto</label>
+              <input type="text" placeholder="0,00" />
+            </InputGroup>
+            <InputGroup>
+              <label>Total</label>
+              <input type="text" placeholder="0,00" />
+            </InputGroup>
+          </FormContainer>
+        </InfoValueBlock>
       </MainBlock>
-      <InfoBlock>
-
-      </InfoBlock>
     </Container>
   );
 }
